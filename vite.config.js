ff971f3050d29d5dd99cf08584dfaab80b1dfc21
MohammadFakih02 <mohammadfakih025@gmail.com>
2025-05-10 22:12:19 +0300

    retrying github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/LU-Senior-Project-Frontend/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
